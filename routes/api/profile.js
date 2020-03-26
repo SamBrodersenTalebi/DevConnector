650b36c584963339fe110961c64c14f542a75d7d
samtalebi <samtalebi84@gmail.com>
2020-03-26 16:59:20 +0100

    delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,4 +146,20 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete a profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //Remove profile
+    await Profile.findByIdAndRemove({ user: req.user.id });
+    //Remove user
+    await User.findByIdAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User removed' });
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
